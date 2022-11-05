57a9dead365d905057e698aa1ac5f164cf16d6c7
yuken02 <git@github.com:yuken02/practice.git>
2022-11-05 09:48:30 +0000

    [Add]3-13 投稿機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
   def new
+    book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to '/books/show'
   end
 
   def index
+    @book = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
