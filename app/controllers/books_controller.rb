55c64262519d6c2c60b8b0bfb65d8ed09b7b2c2f
yuken02 <git@github.com:yuken02/practice.git>
2022-11-07 13:10:58 +0000

    [Add]3-13 createのバリデーションの追加css途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
-
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+	    render :index
+    end
   end
 
   def index
@@ -21,8 +25,12 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
