56a98765937d384da2a822c79608cf66e9bf0fe2
yuken02 <git@github.com:yuken02/practice.git>
2022-11-07 07:51:27 +0000

    [Add]3-13 destroy機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,14 @@
 class BooksController < ApplicationController
-  def new
-    book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to '/books/show'
+    redirect_to book_path(@book.id)
   end
 
   def index
-    @book = Book.all
-    # @book = Book.find(params[:id])
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -20,6 +17,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
